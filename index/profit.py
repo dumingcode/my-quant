fa122b4dc5833a22f209454f9a372d212c34829f
dumingcode <jake1036@126.com>
2018-11-04 14:22:19 +0800

    各个主要指数净利润以及净利润增速展示

--- index/profit.py
+++ index/profit.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-# -*- coding: utf-8 -*-
 """
 Created on Sat Nov  3 14:44:38 2018
-绘图计算各个指数的净利润增长情况
+绘图计算各个指数的净利润增长情况(在同一个图中)
 
 @author: duming
 """
@@ -16,13 +15,18 @@ from pylab import mpl
 def plt_income(arr,bins,index_name):
     mpl.rcParams['font.sans-serif'] = ['FangSong'] # 指定默认字体
     mpl.rcParams['axes.unicode_minus'] = False # 解决保存图像是负号'-'显示为方块的问题
-    plt.rcParams['figure.figsize'] = (16.0, 4.0) # 设置figure_size尺寸
-    fig, (ax0, ax1) = plt.subplots(ncols=2, sharex=True)
-    ax0.set_title(index_name+'净利润总额变化(速度)')
+    plt.rcParams['figure.figsize'] = (12.0, 4.0) # 设置figure_size尺寸
+    fig, ax0 = plt.subplots()
+    ax0.set_title(index_name+'净利润总额及同比增速变化')
+    ax0.set_xlabel('time')
+    ax0.set_ylabel('净利润（亿元）')
     ax0.bar(bins,arr)
-    ax1.set_title(index_name+'净利润增速变化(加速度)')
-    ax1.plot(bins,year_ratio(arr))
-    plt.savefig('../pic/index_profit/'+index_name, dpi=200) #指定分辨率
+    ax2 = ax0.twinx()  # instantiate a second axes that shares the same x-axis
+    color = 'tab:red'
+    ax2.set_ylabel('同比增速%', color=color)  # we already handled the x-label with ax1
+    ax2.plot(bins,year_ratio(arr), color=color)
+    ax2.tick_params(axis='%', labelcolor=color)
+    plt.savefig('../pic/index_profit/'+index_name, dpi=100) #指定分辨率
     plt.show()
 
 # 计算同比增长率
@@ -35,12 +39,12 @@ def year_ratio(arr):
         if(pre == 0):
             ratios.append(0)
         pre = data
-    print(ratios)
     return ratios
             
     
 
 income_pd = pd.read_csv('../data/income.csv',index_col=0)
+income_pd = income_pd / 1e8
 plt_income(income_pd['000016'].values.tolist(),income_pd['000016'].index.tolist(),'上证50')
 plt_income(income_pd['399905'].values.tolist(),income_pd['399905'].index.tolist(),'中证500')
 plt_income(income_pd['000932'].values.tolist(),income_pd['000932'].index.tolist(),'中证消费')
