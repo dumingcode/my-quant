dd32f1fa702d9d344dcc5b7e8f48dad79a0c6086
dumingcode <jake1036@126.com>
2018-11-25 15:49:17 +0800

    roe4张图

--- index/roe/roe.py
+++ index/roe/roe.py
@@ -11,20 +11,45 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pylab import mpl
 
-def plt_income(arr,bins,index_name):
+font = {'family': 'Times New Roman',
+        'color':  'red',
+        'weight': 'normal',
+        'size': 15
+        }
+
+
+def plt_income(arr,bins,arr2,bins2,arr3,bins3,arr4,bins4,index_name):
     mpl.rcParams['font.sans-serif'] = ['FangSong'] # 指定默认字体
     mpl.rcParams['axes.unicode_minus'] = False # 解决保存图像是负号'-'显示为方块的问题
-    plt.rcParams['figure.figsize'] = (16.0, 4.0) # 设置figure_size尺寸
-    fig, (ax0) = plt.subplots(ncols=1)
-    ax0.set_title(index_name+'roe变化')
-    ax0.plot(bins,arr, 'o-')
+    plt.rcParams['figure.figsize'] = (24.0, 8.0) # 设置figure_size尺寸
+    fig, axes = plt.subplots(2, 2)
+    axes[0,0].set_title(index_name+'roe变化')
+    axes[0,0].plot(bins,arr, 'o-')
+    axes[0,1].set_title(index_name+'净利润率变化')
+    axes[0,1].plot(bins2,arr2, 'o-')
+    axes[1,0].set_title(index_name+'杠杆变化')
+    axes[1,0].plot(bins3,arr3, 'o-')
+    axes[1,1].set_title(index_name+'资本周转率变化')
+    axes[1,1].plot(bins4,arr4, 'o-')
     fig.autofmt_xdate()
     cul = 0
     for a, b in zip(bins, arr):
         cul += 1
         if cul % 4 == 0:
-            plt.text(a, b, float('%.2f' % b), ha='center', va='bottom', fontsize=15)
-    plt.savefig('../../pic/index_roe/'+index_name, dpi=200) #指定分辨率
+            axes[0,0].text(a, b, float('%.2f' % b), ha='center', va='bottom', fontsize=15)
+    for a, b in zip(bins2, arr2):
+        cul += 1
+        if cul % 4 == 0:
+            axes[0,1].text(a, b, float('%.2f' % b), ha='center', va='bottom', fontsize=15)
+    for a, b in zip(bins3, arr3):
+        cul += 1
+        if cul % 4 == 0:
+            axes[1,0].text(a, b, float('%.2f' % b), ha='center', va='bottom', fontsize=15)
+    for a, b in zip(bins4, arr4):
+        cul += 1
+        if cul % 4 == 0:
+            axes[1,1].text(a, b, float('%.2f' % b), ha='center', va='bottom', fontsize=15)
+    plt.savefig('../../pic/index_roe/'+index_name, dpi=400) #指定分辨率
     plt.show()
 
 
@@ -42,18 +67,24 @@ def year_roe_cul(roe_pd):
 
 income_pd = pd.read_csv('../../data/index_roe/index_roe.csv',index_col=0)
 roe_pd = year_roe_cul(income_pd)
-plt_income(roe_pd['000016.XSHG'].values.tolist(),roe_pd['000016.XSHG'].index.tolist(),'上证50')
-plt_income(roe_pd['000905.XSHG'].values.tolist(),roe_pd['000905.XSHG'].index.tolist(),'中证500')
-plt_income(roe_pd['000932.XSHG'].values.tolist(),roe_pd['000932.XSHG'].index.tolist(),'中证消费')
-plt_income(roe_pd['000960.XSHG'].values.tolist(),roe_pd['000960.XSHG'].index.tolist(),'中证龙头')
-plt_income(roe_pd['000922.XSHG'].values.tolist(),roe_pd['000922.XSHG'].index.tolist(),'中证红利')
-plt_income(roe_pd['000300.XSHG'].values.tolist(),roe_pd['000300.XSHG'].index.tolist(),'沪深300')
-plt_income(roe_pd['399006.XSHE'].values.tolist(),roe_pd['399006.XSHE'].index.tolist(),'创业板')
-plt_income(roe_pd['399701.XSHE'].values.tolist(),roe_pd['399701.XSHE'].index.tolist(),'深证F60')
-plt_income(roe_pd['000978.XSHG'].values.tolist(),roe_pd['000978.XSHG'].index.tolist(),'医药100')
-plt_income(roe_pd['399005.XSHE'].values.tolist(),roe_pd['399005.XSHE'].index.tolist(),'中小板')
+profit_pd = pd.read_csv('../../data/index_roe/index_net_profit_margin.csv',index_col=0)
+level_pd = pd.read_csv('../../data/index_roe/index_level.csv',index_col=0)
+roa_pd = pd.read_csv('../../data/index_roe/index_roa.csv',index_col=0)
 
+turnover_pd = roa_pd/profit_pd*100
+turnover_pd = turnover_pd.fillna(0)
 
+plt_income(roe_pd['000016.XSHG'].values.tolist(),roe_pd['000016.XSHG'].index.tolist(),profit_pd['000016.XSHG'].values.tolist(),profit_pd['000016.XSHG'].index.tolist(),level_pd['000016.XSHG'].values.tolist(),level_pd['000016.XSHG'].index.tolist(),turnover_pd['000016.XSHG'].values.tolist(),turnover_pd['000016.XSHG'].index.tolist(),'上证50')
+plt_income(roe_pd['000905.XSHG'].values.tolist(),roe_pd['000905.XSHG'].index.tolist(),profit_pd['000905.XSHG'].values.tolist(),profit_pd['000905.XSHG'].index.tolist(),level_pd['000905.XSHG'].values.tolist(),level_pd['000905.XSHG'].index.tolist(),turnover_pd['000905.XSHG'].values.tolist(),turnover_pd['000905.XSHG'].index.tolist(),'中证500')
+plt_income(roe_pd['000932.XSHG'].values.tolist(),roe_pd['000932.XSHG'].index.tolist(),profit_pd['000932.XSHG'].values.tolist(),profit_pd['000932.XSHG'].index.tolist(),level_pd['000932.XSHG'].values.tolist(),level_pd['000932.XSHG'].index.tolist(),turnover_pd['000932.XSHG'].values.tolist(),turnover_pd['000932.XSHG'].index.tolist(),'中证消费')
+plt_income(roe_pd['000960.XSHG'].values.tolist(),roe_pd['000960.XSHG'].index.tolist(),profit_pd['000960.XSHG'].values.tolist(),profit_pd['000960.XSHG'].index.tolist(),level_pd['000960.XSHG'].values.tolist(),level_pd['000960.XSHG'].index.tolist(),turnover_pd['000960.XSHG'].values.tolist(),turnover_pd['000960.XSHG'].index.tolist(),'中证红利')
+plt_income(roe_pd['000978.XSHG'].values.tolist(),roe_pd['000978.XSHG'].index.tolist(),profit_pd['000978.XSHG'].values.tolist(),profit_pd['000978.XSHG'].index.tolist(),level_pd['000978.XSHG'].values.tolist(),level_pd['000978.XSHG'].index.tolist(),turnover_pd['000978.XSHG'].values.tolist(),turnover_pd['000978.XSHG'].index.tolist(),'医药100')
+
+plt_income(roe_pd['399005.XSHE'].values.tolist(),roe_pd['399005.XSHE'].index.tolist(),profit_pd['399005.XSHE'].values.tolist(),profit_pd['399005.XSHE'].index.tolist(),level_pd['399005.XSHE'].values.tolist(),level_pd['399005.XSHE'].index.tolist(),turnover_pd['399005.XSHE'].values.tolist(),turnover_pd['399005.XSHE'].index.tolist(),'中小板')
+plt_income(roe_pd['000960.XSHG'].values.tolist(),roe_pd['000960.XSHG'].index.tolist(),profit_pd['000960.XSHG'].values.tolist(),profit_pd['000960.XSHG'].index.tolist(),level_pd['000960.XSHG'].values.tolist(),level_pd['000960.XSHG'].index.tolist(),turnover_pd['000960.XSHG'].values.tolist(),turnover_pd['000960.XSHG'].index.tolist(),'中证龙头')
+plt_income(roe_pd['000300.XSHG'].values.tolist(),roe_pd['000300.XSHG'].index.tolist(),profit_pd['000300.XSHG'].values.tolist(),profit_pd['000300.XSHG'].index.tolist(),level_pd['000300.XSHG'].values.tolist(),level_pd['000300.XSHG'].index.tolist(),turnover_pd['000300.XSHG'].values.tolist(),turnover_pd['000300.XSHG'].index.tolist(),'沪深300')
+plt_income(roe_pd['399006.XSHE'].values.tolist(),roe_pd['399006.XSHE'].index.tolist(),profit_pd['399006.XSHE'].values.tolist(),profit_pd['399006.XSHE'].index.tolist(),level_pd['399006.XSHE'].values.tolist(),level_pd['399006.XSHE'].index.tolist(),turnover_pd['399006.XSHE'].values.tolist(),turnover_pd['399006.XSHE'].index.tolist(),'创业板')
+plt_income(roe_pd['399701.XSHE'].values.tolist(),roe_pd['399701.XSHE'].index.tolist(),profit_pd['399701.XSHE'].values.tolist(),profit_pd['399701.XSHE'].index.tolist(),level_pd['399701.XSHE'].values.tolist(),level_pd['399701.XSHE'].index.tolist(),turnover_pd['399701.XSHE'].values.tolist(),turnover_pd['399701.XSHE'].index.tolist(),'深证F60')
 
 
 
